f7a63b91d2f94be5056d2d1ea5cc379b4adf3b92
Wycliff Ochieng <wyckieochieng91@gmail.com>
2024-12-12 07:17:23 +1200

    Update lab4_fact.sql

--- lab4_fact.sql
+++ lab4_fact.sql
@@ -41,5 +41,32 @@ join games g on gd.game_id = g.game_id
  from deduped where row_number=1;
  
 create table fact_table_details(
-
+dim_game_date date,
+dim_season integer,
+dim_team_id integer,
+dim_player_id integer,
+dim_player_name text,
+dim_start_position text,
+dim_is_playing_at_home boolean,
+dim_did_not_play boolean,
+dim_did_not_dress boolean,
+dim_not_with_team boolean,
+m_minutes integer,
+m_fgm integer,
+m_fga integer,
+m_fg3m integer,
+m_fg3a integer,
+m_ftm integer,
+m_fta integer,
+m_oreb integer,
+m_dreb integer,
+m_reb integer,
+m_ast integer,
+m_stl integer,
+m_blk integer,
+m_turnovers integer,
+m_pf integer,
+m_pts integer,
+m_plus_minus integer,
+primary key(dim_game_date,dim_team_id,dim_player_id)
 );
